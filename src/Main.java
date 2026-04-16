fbd7a6df86a1c0ef4d981f98b7b5499f6dd6e8ae
asif-kamal <asif.kamal1489@gmail.com>
2026-04-16 16:29:42 -0500

    practice beginners Java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int myVariable = 50;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(
+                myVariable == 0 ? "it's now 0" : "it's not 0"
+        );
+
+        if (myVariable == 0) System.out.println("It is 0"); else System.out.println("It is not 0");
+
+        System.out.println(calculateScore(2, 5));
+    }
+
+    public static String calculateScore(int num1, int num2) {
+        return "Score is " + (num1 + num2);
     }
 }
