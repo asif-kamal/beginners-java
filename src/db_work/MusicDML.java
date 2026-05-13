8e8cea844c64d8b17607deffe664b17785d8a505
asif-kamal <asif.kamal1489@gmail.com>
2026-05-13 14:53:33 -0500

    execute() returns true whether an artist is found or not

--- src/db_work/MusicDML.java
+++ src/db_work/MusicDML.java
@@ -9,22 +9,19 @@ public class MusicDML {
 
     public static void main(String[] args) {
 
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
         try (Connection conn = DriverManager.getConnection(
                 "jdbc:postgresql://localhost:5432/music",
                 System.getenv("USER"),
                 System.getenv("PASSWORD"));
              Statement statement = conn.createStatement()
         ) {
-            String artist = "Elf";
+            String artist = "Neil Young";
             String query = "SELECT * FROM artists WHERE artist_name='%s'".formatted(artist);
             boolean result = statement.execute(query);
+            var rs = statement.getResultSet();
+            boolean found = (rs!=null && rs.next());
             System.out.println("Result: " + result);
+            System.out.println("Artist was " + (found? "found" : "not found"));
         } catch (SQLException e) {
             e.printStackTrace();
         }
